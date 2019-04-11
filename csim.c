a45b0de2e70aafece0ab90e6b8e24c2abe872d7d
ksasakipz <ksasaki@students.pitzer.edu>
2019-04-10 20:48:05 -0700

    hit and misses might work?

--- csim.c
+++ csim.c
@@ -12,6 +12,8 @@ Riley Mangan, Harris McCullers, and Kevin Sasaki
 #include <stdlib.h>
 #include <unistd.h>
 
+#define cache_size 10
+
 //If l is the length of the address (in bits), then the number of tag bits is t = l − b − s
 //t = 64 - b - s
 struct set{
@@ -19,32 +21,29 @@ struct set{
     int index, priority;
 };
 
+
+//TODO make a constructor to make cache_size equal to whatever our input was from the command line
 struct cache{
     int hits, misses, evictions;
-    struct set our_sets[10];
+    struct set * our_sets;
 };
 
-// struct obj load(struct cache our_cache, int address, int size){
-//     return cache;
-// }
+//cache constructor
+struct cache * cache_new(int x) {
+    struct cache *c = malloc(sizeof *c);
+    c->hits = 0;
+    c->misses = 0;
+    c->evictions = 0;
+    struct set s[x];
+    c->our_sets = s;
+    return c;
+}
 
-// struct obj store(struct cache our_cache, int address, int size){
-//     return cache;
-// }
-int in_cache(struct cache * our_cache, char * address){
-    int i;
-    for(i = 0; i < sizeof(our_cache->our_sets)/sizeof(our_cache->our_sets[0]); i++){
-        if(our_cache->our_sets[i].address == address){
-            return 1;
-        }
-    }
-    return 0;
-};
+/* delete struct */
+void cache_delete(struct cache * c) {
+    free(c);
+}
 
-//is something like this necessary?
-//the idea is that we keep track of the order that the sets came in with an int priority in the set struct
-//the next set to get kicked out is one with a priority number equal to the number of sets
-//each priority number gets updated in the function, so anytime we modify the cache, we call this
 struct cache* update_priority(struct cache * our_cache, struct set* our_set){
     int old_priority = our_set->priority;
     for(int i = 0; i < sizeof(our_cache->our_sets)/sizeof(our_cache->our_sets[0]); i++){
@@ -59,10 +58,35 @@ struct cache* update_priority(struct cache * our_cache, struct set* our_set){
     return our_cache;
 }
 
-struct cache * helper_func(char * operation, char * address, int size, struct cache * our_cache){
-    int opt = (*operation);
-    int add = 0;
+//supposed 
+struct cache * place_in_cache(struct cache * our_cache, char * address){
+    for(int i = 0; i < cache_size; i++){
+        if(our_cache->our_sets[i].priority == cache_size){
+            update_priority(our_cache, &our_cache->our_sets[i]);
+            our_cache->our_sets[i].address = address;
+            return our_cache;
+        }
+    }
+    return our_cache;
+}
 
+//returns 1 if the parameter address is in the our_cache
+int in_cache(struct cache * our_cache, char * address){
+    int i;
+    for(i = 0; i < cache_size; i++){
+        char * comparison_address = our_cache->our_sets[i].address;
+
+        if(comparison_address == address){
+            printf("hello\n");
+            return 1;
+        }
+    }
+    return 0;
+};
+
+struct cache * helper_func(char * operation, char * address, int size, struct cache * our_cache){
+    int opt = *operation;
+    int add = 0;
     //adjust hits an misses for our cache in this switch statement
     switch(opt){
         case 'I':
@@ -70,18 +94,24 @@ struct cache * helper_func(char * operation, char * address, int size, struct ca
         case 'S':
             //if a miss, we need to add the new block to the cache
             add = in_cache(our_cache, address);
-            our_cache->hits += add;
-            our_cache->misses += 1 - add;
+            printf("add: %d\n", add);
+            our_cache->hits = our_cache->hits + add;
+            our_cache->misses = our_cache->misses + 1 - add;
 
             //if a hit, we need to update the priority numbers
             break;
         case 'L':
             add = in_cache(our_cache, address);
-            our_cache->hits += add;
-            our_cache->misses += 1 - add;
+            our_cache = place_in_cache(our_cache, address);
+            printf("add: %d\n", add);
+            our_cache->hits = our_cache->hits + add;
+            our_cache->misses = our_cache->misses + 1 - add;
+            printf("our_cache->misses: %d\n", our_cache->misses);
             break;
         case 'M':
             add = in_cache(our_cache, address);
+            printf("add: %d\n", add);
+            our_cache->hits += 1;
             our_cache->hits += add;
             our_cache->misses += 1 - add;
             break;
@@ -124,21 +154,23 @@ int main(int argc, char **argv)
     }
 
     //allocate enough space for our cache
-    struct cache * our_cache = malloc(s*E*b);
+    struct cache * our_cache = cache_new(s);
+    
 
     //read file line by line from text file and process
     FILE *file = fopen(t, "r");
     char operation[2], address[8];
     int size;
     while(fscanf(file, " %s %s, %d", operation, address, &size) != -1){
-        our_cache = helper_func(operation, address, size, our_cache);
+        our_cache = helper_func(&operation[0], &address[0], size, our_cache);
+        printf("operation, address, size: %s %s %d\n", operation, address, size);
     }
-    printf("%s %s %d\n", operation, address, size);
+    
 
     printf("s, E, b, t: %d %d %d %s\n",s,E,b,t);
 
-    printSummary(0, 0, 0);
+    printSummary(our_cache->hits, our_cache->misses, our_cache->evictions);
 
-    free(our_cache);
+    cache_delete(our_cache);
     return 0;
 }
